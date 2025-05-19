c3d436c4a96234651cc5092304adde5ba4feebfb
schweigenderFlugel <facundoezcastro@gmail.com>
2025-05-19 00:52:39 -0300

    add new fields

--- server/api/models/students.py
+++ server/api/models/students.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Optional
 from enum import Enum
 from uuid import UUID, uuid4
@@ -17,25 +18,27 @@ class TipoNEAE(str, Enum):
 
 class StudentBase(SQLModel):
   id_estudiante: int = Field(sa_column=Column(pg.INTEGER, unique=True))
-  legajo: int = Field(sa_column=Column(pg.INTEGER, unique=True))
-  genero: Genero = Field(sa_column=Column(pg.ENUM(Genero))) 
-  grado: int = Field(sa_column=Column(pg.INTEGER)) 
-  asistencia_inicial: int = Field(sa_column=Column(pg.INTEGER))
-  asistencia: int = Field(sa_column=Column(pg.INTEGER)) 
-  calificacion_matematica: int = Field(sa_column=Column(pg.INTEGER)) 
-  calificacion_lengua: int = Field(sa_column=Column(pg.INTEGER)) 
-  horas_clase_semanales: int = Field(sa_column=Column(pg.INTEGER)) 
-  motivacion: int = Field(sa_column=Column(pg.INTEGER)) 
-  habilidades_de_autoregulacion: int = Field(sa_column=Column(pg.INTEGER)) 
-  habilidades_interpersonales: int = Field(sa_column=Column(pg.INTEGER)) 
-  habilidades_intrapersonales: int = Field(sa_column=Column(pg.INTEGER)) 
-  conducta_riesgo: int = Field(sa_column=Column(pg.INTEGER)) 
-  libros_en_casa: int = Field(sa_column=Column(pg.INTEGER)) 
-  internet_en_casa: int = Field(sa_column=Column(pg.INTEGER)) 
-  distancia_escuela_km: int = Field(sa_column=Column(pg.INTEGER)) 
-  clima_escolar: int = Field(sa_column=Column(pg.INTEGER)) 
-  capacitacion_docente_anual_horas: int = Field(sa_column=Column(pg.INTEGER)) 
-  tenencia_director_anos: int = Field(sa_column=Column(pg.INTEGER)) 
+  legajo: str = Field(sa_column=Column(pg.VARCHAR, unique=True), max_length=10)
+  nombre: str = Field(sa_column=Column(pg.VARCHAR), max_length=30, description='Tiene que ser nombre completo')
+  edad: int = Field(sa_column=Column(pg.SMALLINT), max_length=10)
+  genero: Genero = Field(sa_column=Column(pg.ENUM(Genero)))
+  grado: int = Field(sa_column=Column(pg.INTEGER))
+  asistencia_inicial: bool = Field(sa_column=Column(pg.BOOLEAN))
+  asistencia: int = Field(sa_column=Column(pg.INTEGER))
+  calificacion_matematica: float = Field(sa_column=Column(pg.FLOAT))
+  calificacion_lengua: float = Field(sa_column=Column(pg.FLOAT))
+  horas_clase_semanales: int = Field(sa_column=Column(pg.INTEGER))
+  motivacion: int = Field(sa_column=Column(pg.INTEGER))
+  habilidades_de_autoregulacion: int = Field(sa_column=Column(pg.INTEGER))
+  habilidades_interpersonales: int = Field(sa_column=Column(pg.INTEGER))
+  habilidades_intrapersonales: int = Field(sa_column=Column(pg.INTEGER))
+  conducta_riesgo: int = Field(sa_column=Column(pg.INTEGER))
+  libros_en_casa: int = Field(sa_column=Column(pg.INTEGER))
+  internet_en_casa: int = Field(sa_column=Column(pg.INTEGER))
+  distancia_escuela_km: int = Field(sa_column=Column(pg.INTEGER))
+  clima_escolar: int = Field(sa_column=Column(pg.INTEGER))
+  capacitacion_docente_anual_horas: int = Field(sa_column=Column(pg.INTEGER))
+  tenencia_director_anos: int = Field(sa_column=Column(pg.INTEGER))
   adecuaciones_curriculare: int = Field(Integer, nullable=False)
   tipo_nea: TipoNEAE = Field(sa_column=Column(pg.ENUM(TipoNEAE)))
   violencia_familia: int = Field(sa_column=Column(pg.INTEGER))
@@ -46,6 +49,11 @@ class StudentBase(SQLModel):
     
 class Student(StudentBase, table=True):
   id: Optional[UUID] = Field(sa_column=Column(pg.UUID, nullable=True, primary_key=True), default=uuid4())
+  createdAt: Optional[int] = Field(sa_column=Column(pg.DATE, nullable=True), default=datetime.timestamp(datetime.now()))
+  updatedAt: Optional[int] = Field(sa_column=Column(pg.DATE, nullable=True), default=datetime.timestamp(datetime.now()))
 
 class StudentCreate(StudentBase):
+  pass
+
+class StudentUpdate(Optional[StudentBase]):
   pass
--- server/api/routes/consultancy.py
+++ server/api/routes/consultancy.py
@@ -1,24 +1,16 @@
 from fastapi import APIRouter, Depends, Body, HTTPException
-# from fastapi.responses import JSONResponse
-from dotenv import load_dotenv
 from sqlalchemy.exc import IntegrityError
-# from fastapi.encoders import jsonable_encoder
 from models.students import Student, StudentCreate
 from db import DatabaseDep
 from clerk import AuthDep
-import os
-
-load_dotenv()
-
-CLERK_SECRET_KEY = os.getenv('CLERK_SECRET_KEY')
-CLERK_JWKS_URL = os.getenv('CLERK_JWKS_URL')
+from utils.csv_utils import csv_file
 
 router = APIRouter(
   tags=['Consultancy'],
   prefix='/consultancy',
 )
 
-@router.post("", tags=['Consultancy'], summary='Consultancy')
+@router.post('', status_code=201, tags=['Consultancy'], summary='Consultancy')
 async def consultancy(
   auth: AuthDep,
   session: DatabaseDep,
@@ -29,9 +21,34 @@ async def consultancy(
     session.add(consult)
     session.commit()
     session.refresh(consult)
+    keys = [k for k in consult.model_dump().keys()]
+    values = [v for v in consult.model_dump().values()]
+    csv_file(keys, values)
     return { "message": 'Consult successfully created!' }
   except Exception as e:
     if isinstance(e, IntegrityError) and "duplicate key value violates unique constraint" in str(e.orig):  # PostgreSQL
       raise HTTPException(status_code=400, detail="The consult already exists!")
     else:
       raise HTTPException(status_code=500, detail='Unexpected error!')
+    
+@router.post('', status_code=201, tags=['Consultancy'], summary='Consultancy')
+async def consultancy(
+  auth: AuthDep,
+  session: DatabaseDep,
+  body: StudentCreate = Body(),
+):
+  try:
+    consult = Student.model_validate(body.model_dump())
+    session.add(consult)
+    session.commit()
+    session.refresh(consult)
+    keys = [k for k in consult.model_dump().keys()]
+    values = [v for v in consult.model_dump().values()]
+    csv_file(keys, values)
+    return { "message": 'Consult successfully created!' }
+  except Exception as e:
+    if isinstance(e, IntegrityError) and "duplicate key value violates unique constraint" in str(e.orig):  # PostgreSQL
+      raise HTTPException(status_code=400, detail="The consult already exists!")
+    else:
+      raise HTTPException(status_code=500, detail='Unexpected error!')
+    

978d3e8bbdad9fc759a6fd7af46d7d9ed094b2f4
Nicolás Enciso <enectrl@gmail.com>
2025-05-21 00:11:24 -0300

    fix: protected routes home redirect

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -11,7 +11,7 @@ const ProtectedRoute = () => {
   const { isSignedIn, isLoaded } = useAuth();
 
   if (!isLoaded) return <Loading />;
-  if (isSignedIn) return <Navigate to="/iniciar" />;
+  if (!isSignedIn) return <Navigate to="/home" />;
 
   return <Outlet />;
 };
--- client/src/router/routes.tsx
+++ client/src/router/routes.tsx
@@ -1,19 +1,33 @@
 import { Route, Routes } from "react-router";
 import HomePage from "@pages/HomePage";
 import ProtectedRoute from "@/components/ProtectedRoute";
-import LoginPage from "@/pages/LoginPage";
+import ConsultancyPage from "@/pages/ConsultancyPage";
+import ReportingPage from "@/pages/ReportingPage";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { useUser } from "@clerk/clerk-react";
 
 /**
  * Componente de rutas de la aplicación.
  */
 const AppRoutes = () => {
+  const { isSignedIn } = useUser();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isSignedIn) {
+      navigate("/", { replace: true });
+    }
+  }, [isSignedIn, navigate]);
+
   return (
     <Routes>
       <Route element={<ProtectedRoute />}>
-        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/" element={<ConsultancyPage />} />
+        <Route path="reporte" element={<ReportingPage />} />
       </Route>
-      <Route path="iniciar" element={<LoginPage />} />
-      <Route path="*" element={<LoginPage />} />
+      <Route path="home" element={<HomePage />} />
+      <Route path="*" element={<HomePage />} />
     </Routes>
   );
 };

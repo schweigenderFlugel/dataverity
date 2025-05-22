c62ac627e86a20d0987b85dce76e1b70fd82039d
schweigenderFlugel <facundoezcastro@gmail.com>
2025-05-22 02:28:53 -0300

    fixes on main and clerk

--- server/api/clerk.py
+++ server/api/clerk.py
@@ -9,19 +9,19 @@ load_dotenv()
 
 CLERK_SECRET_KEY = os.getenv('CLERK_SECRET_KEY')
 
+FRONTEND_URL = os.getenv('FRONTEND_URL')
+
 def protected_route(request: Request):
   clerk = Clerk(bearer_auth=CLERK_SECRET_KEY)
   request_state = clerk.authenticate_request(
     request,
     AuthenticateRequestOptions(
-      authorized_parties=['https://example.com']
+      authorized_parties=[FRONTEND_URL]
     )
   )
 
   if not request_state.is_signed_in:
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Missing or invalid Authorization header")
-
-  token = request_state
 
   return request_state
 
--- server/api/main.py
+++ server/api/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
   CORSMiddleware,
   allow_origins=[BACKEND_URL, FRONTEND_URL],
   allow_credentials=True,
-  allow_methods=["GET,POST,PUT,DELETE"],
+  allow_methods=["GET", "POST", "PUT", "DELETE"],
   allow_headers=["*"],
 )
 
